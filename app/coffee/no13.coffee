51379b69fcf4b18c09e9e8ab134071c6b186fa92
Hong Nguyen <hong.nguyen@gsmartsolutions.com>
2017-03-03 09:22:53 +0700

    Guard files

---
+++ app/coffee/no13.coffee
@@ -0,0 +1,57 @@
+getProducts = (tag) ->
+  $.get 'products.json', (data) ->
+    $.get 'product.html', (template) ->
+      products = data.products.map((product) ->
+        template.template product
+      )
+      $('.products').append products
+      changeProducts tag
+      return
+    return
+  return
+
+changeProducts = (tag) ->
+  $('.products').isotope
+    itemSelector: '.product'
+    filter: if tag == '#all' then '*' else tag.replace('#', '.')
+  return
+
+String::template = (params) ->
+  self = this
+  switch typeof self
+    when 'string'
+      return
+    when 'object'
+      for key of params
+        self = self.replace(eval('/{{' + key + '}}/g'), params[key])
+      return self
+  return
+
+$(document).ready(->
+  setTimeout (->
+    $('.logo').css(
+      top: $('.logo').offset().top
+      left: $('.logo').offset().left
+      position: 'fixed'
+      'z-index': 1000).appendTo($('#page-content .left')).animate {
+      left: '50px'
+      top: '30px'
+    }, 500, ->
+      $('.nav ul').slideDown()
+      window.tag = location.hash or '#all'
+      $('.nav a[href=\\' + tag + ']').parent().addClass 'active'
+      getProducts tag
+      return
+    $('#page-container').fadeOut 'fast', ->
+      $('#page-container').remove()
+      return
+    return
+  ), 0
+  return
+).on 'click', '.nav a', (e) ->
+  e.preventDefault()
+  window.history.pushState null, $(this).html(), @href
+  $(this).parent().parent().children('.active').removeClass 'active'
+  $(this).parent().addClass 'active'
+  changeProducts window.location.hash
+  false
--- javascript/no13.js
+++ javascript/no13.js
@@ -1,62 +1,67 @@
-String.prototype.template = function(params){
-  self = this
-  switch(typeof self){
-    case 'string':
-      break
-    case 'object':
-      for (var key in params){
-        self = self.replace(eval('/\{\{'+key+'}}/g'), params[key])
-      }
-      return self
-  }
+(function() {
+  var changeProducts, getProducts;
 
-}
+  getProducts = function(tag) {
+    $.get('products.json', function(data) {
+      $.get('product.html', function(template) {
+        var products;
+        products = data.products.map(function(product) {
+          return template.template(product);
+        });
+        $('.products').append(products);
+        changeProducts(tag);
+      });
+    });
+  };
 
-function getProducts(tag){
-  $.get('products.json', function(data){
-    $.get('product.html', function(template){
-      products = data.products.map(function(product){
-        return template.template(product)
-      })
-      $('.products').append(products)
-      changeProducts(tag)
-    })
-  })
-}
+  changeProducts = function(tag) {
+    $('.products').isotope({
+      itemSelector: '.product',
+      filter: tag === '#all' ? '*' : tag.replace('#', '.')
+    });
+  };
 
-function changeProducts(tag){
-  $('.products').isotope({
-    itemSelector: '.product',
-    filter: tag == '#all' ? '*' : tag.replace('#', '.')
-  })
-}
+  String.prototype.template = function(params) {
+    var key, self;
+    self = this;
+    switch (typeof self) {
+      case 'string':
+        return;
+      case 'object':
+        for (key in params) {
+          self = self.replace(eval('/{{' + key + '}}/g'), params[key]);
+        }
+        return self;
+    }
+  };
 
-$(document).ready(function(){
-  setTimeout(function(){
-    $('.logo').css({
-      top: $('.logo').offset().top,
-      left: $('.logo').offset().left,
-      position: 'fixed',
-      'z-index': 1000
-    }).appendTo($('#page-content .left')).animate({
-      left: '50px',
-      top: '30px'
-    }, 500, function(){
-      $('.nav ul').slideDown()
-      window.tag = location.hash || '#all'
-      $('.nav a[href=\\'+tag+']').parent().addClass('active')
-      getProducts(tag)
-    })
-    $("#page-container").fadeOut('fast', function(){
-      $("#page-container").remove()
-    })
-  }, 0)
-})
-.on('click', '.nav a', function(e){
-  e.preventDefault()
-  window.history.pushState(null, $(this).html(), this.href)
-  $(this).parent().parent().children('.active').removeClass('active')
-  $(this).parent().addClass('active')
-  changeProducts(window.location.hash)
-  return false
-})
+  $(document).ready(function() {
+    setTimeout((function() {
+      $('.logo').css({
+        top: $('.logo').offset().top,
+        left: $('.logo').offset().left,
+        position: 'fixed',
+        'z-index': 1000
+      }).appendTo($('#page-content .left')).animate({
+        left: '50px',
+        top: '30px'
+      }, 500, function() {
+        $('.nav ul').slideDown();
+        window.tag = location.hash || '#all';
+        $('.nav a[href=\\' + tag + ']').parent().addClass('active');
+        getProducts(tag);
+      });
+      $('#page-container').fadeOut('fast', function() {
+        $('#page-container').remove();
+      });
+    }), 0);
+  }).on('click', '.nav a', function(e) {
+    e.preventDefault();
+    window.history.pushState(null, $(this).html(), this.href);
+    $(this).parent().parent().children('.active').removeClass('active');
+    $(this).parent().addClass('active');
+    changeProducts(window.location.hash);
+    return false;
+  });
+
+}).call(this);
